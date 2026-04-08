c54faa0a9671f3f5093436ca95d4a3187baca8a7
HasanTalukderIctian <hasantalukdercou@gmail.com>
2026-04-08 18:22:33 +0600

    Swiper Card Added

--- src/assets/Frontend/AboutUs.jsx
+++ src/assets/Frontend/AboutUs.jsx
@@ -3,12 +3,26 @@ import Header from './Common/Header';
 import Footer from './Common/Footer';
 import '../css/about.css';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper/modules';
+
+import "swiper/css";
+import "swiper/css/pagination";
+
 // ইমেজের পাথগুলো আপনার প্রজেক্ট অনুযায়ী ইমপোর্ট করুন
 import king1 from '../image/King-room1.jpg';
 import king2 from '../image/King-room2.jpg';
 import king3 from '../image/King-room13.jpg';
 import king4 from '../image/King-room4.jpg';
 import king5 from '../image/king5.jpg';
+
+
+import img1 from '../image/facility1.jpg';
+import img2 from '../image/facility2.jpg';
+import img3 from '../image/facility3.jpg';
+import img4 from '../image/facility4.jpg';
+import img5 from '../image/facility5.jpg';
+
 
 const AboutUs = () => {
     // ইমেজ রিপ্লেস করার জন্য স্টেট
@@ -24,7 +38,15 @@ const AboutUs = () => {
         { id: 4, img: king4, title: "City View" },
         { id: 5, img: king5, title: "Family Room" },
     ];
+    
 
+    const facilities = [
+        { id: 1, title: "Airport Taxi", img: img1, icon: "🚕" },
+        { id: 2, title: "Swimming Pool", img: img2, icon: "🏊" },
+        { id: 3, title: "Spa Center", img: img3, icon: "🧖‍♀️" },
+        { id: 4, title: "Fitness Center", img: img4, icon: "🏋️‍♂️" },
+        { id: 5, title: "Indoor Games", img: img5, icon: "🎮" }, // উদাহরণের জন্য ডুপ্লিকেট
+    ];
 
     return (
         <>
@@ -119,6 +141,55 @@ const AboutUs = () => {
                     </div>
                 </div>
             </section>
+
+            {/* 3rd Section */}
+
+            <section className="facility-slider-section py-5">
+            <div className="container">
+                <div className="text-center mb-5">
+                    <span className="text-gold text-uppercase ls-2 small fw-bold">Facilities</span>
+                    <h2 className="serif display-6 mt-2">Our Hotelux <span className="text-muted">Facilities</span></h2>
+                </div>
+
+                <Swiper
+                    modules={[Autoplay, Pagination]}
+                    spaceBetween={30}
+                    slidesPerView={1}
+                    loop={true} // বৃত্তাকার মুভিং (Circular)
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                 
+                    breakpoints={{
+                        640: { slidesPerView: 2 },
+                        1024: { slidesPerView: 4 },
+                    }}
+                    className="facility-swiper"
+                >
+                    {facilities.map((item) => (
+                        <SwiperSlide key={item.id}>
+                            <div className="facility-card shadow-sm bg-white">
+                                <div className="facility-img-wrapper position-relative">
+                                    <img src={item.img} alt={item.title} className="img-fluid" />
+                                    {/* গোল আইকন হোল্ডার */}
+                                    <div className="facility-icon-circle">
+                                        <span className="fs-4">{item.icon}</span>
+                                    </div>
+                                </div>
+                                <div className="facility-content text-center p-2">
+                                    <h4 className="serif mb-3">{item.title}</h4>
+                                    <p className="text-muted small">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    </p>
+                                    <button className="read-more-btn mt-3 py-1 px-2">Read More</button>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </section>
 
             <Footer />
         </>
